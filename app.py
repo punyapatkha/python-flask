1ef9c6ccbcc66bfa84c125ae733b49fa2c1059af
Pooh <71396594+punyapatkha@users.noreply.github.com>
2022-07-04 14:47:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,21 @@ app = Flask(__name__)
 def hello():
     return 'Hello World!'
 
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        print("Data received from Webhook is: ", request.json)
+        return "Webhook received!"
+    
+    
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+    
+    
+    
+
+
+
+
